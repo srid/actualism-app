2cca5542058e483ae28595ee708bd2a8439f056b
Sridhar Ratnakumar <srid@srid.ca>
2024-11-07 10:21:04 -0500

    css: Add "srid" styling
    Useful to describe personal stuff decoupled from general explanation

--- src/ActualismApp/Style.hs
+++ src/ActualismApp/Style.hs
@@ -30,6 +30,12 @@ style = do
     border (px 1) solid black
     borderRadius (px 5) (px 5) (px 5) (px 5)
     boxShadow $ one $ bsColor darkslateblue $ shadow (px 7) (px 7)
+  div # ".srid" ? do
+    fontSize $ em 0.8
+    opacity 0.2
+    ":hover" & do
+      opacity 1
+    paddingLeft (px 20)
   span # ".definition-link" ? do
     color blueviolet
     cursor pointer
