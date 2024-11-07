b2d4412cc10e9bd22164f141ed79219ec6fdb07a
Sridhar Ratnakumar <srid@srid.ca>
2024-11-07 15:25:29 -0500

    add deploy key

--- nix/modules/flake-parts/nixos/configuration.nix
+++ nix/modules/flake-parts/nixos/configuration.nix
@@ -10,7 +10,10 @@
   networking.domain = "";
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
+    # srid
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd''
+    # Deploy key
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdDeo43SrqSQbqm3BdT778KCz26G4xhINa8559gQs3K''
   ];
   system.stateVersion = "23.11";
 }
